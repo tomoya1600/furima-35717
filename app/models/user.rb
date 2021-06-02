64938b0c36239ace4888ca0c04da3f2a7bd0bbb6
tomoya1600 <on100.ogata.tomoya@gmail.com>
2021-06-02 20:20:54 +0900

    rspec導入

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,10 +4,12 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  validates :nickname,        presence: true
-  validates :last_name,       presence: true
-  validates :first_name,      presence: true
-  validates :last_name_kana,  presence: true
-  validates :first_name_kana, presence: true
-  validates :birthday,        presence: true
+  validates :nickname,           presence: true
+  validates :encrypted_password, presence: true
+  validates :last_name,          presence: true
+  validates :first_name,         presence: true
+  validates :last_name_kana,     presence: true
+  validates :first_name_kana,    presence: true
+  validates :birthday,           presence: true
+
 end

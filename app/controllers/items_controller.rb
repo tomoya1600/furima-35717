8e4063f983a9e72e7c2d8f126623fbc0e46f867f
tomoya1600 <on100.ogata.tomoya@gmail.com>
2021-06-04 17:46:32 +0900

    アクション、バリデーション、アソシエーション

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,9 +1,19 @@
 class ItemsController < ApplicationController
   def index
+    @items = Item.order("created_at DESC")
   end
 
   def new
     @item = Item.new
+  end
+
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
   end
 
   private

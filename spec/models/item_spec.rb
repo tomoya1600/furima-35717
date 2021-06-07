f7b3bc3d26098d45e1be695e8f0e146d6284b1fd
tomoya1600 <on100.ogata.tomoya@gmail.com>
2021-06-07 17:42:53 +0900

    テストコード指摘箇所の修正

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -86,12 +86,17 @@ RSpec.describe Item, type: :model do
       it 'priceが全角英字では出品できない' do
         @item.price = 'ａａａａ'
         @item.valid?
-        expect(@item.errors.full_messages).to include
+        expect(@item.errors.full_messages).to include("Price is not a number")
       end
       it 'imageが空では出品できない' do
         @item.image = nil
         @item.valid?
         expect(@item.errors.full_messages).to include("Image can't be blank")
+      end
+      it 'userが紐づいていないと出品できない' do
+        @item.user = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include("User must exist")
       end
     end
   end

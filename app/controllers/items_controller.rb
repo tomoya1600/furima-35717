c63f353e4add23ee106549766ec93317d07e070b
tomoya1600 <on100.ogata.tomoya@gmail.com>
2021-06-08 18:35:40 +0900

    ルーティング、アクションの設定

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -24,9 +24,16 @@ class ItemsController < ApplicationController
 
   def edit
     @item = Item.find(params[:id])
+    redirect_to root_path if current_user.id != @item.user_id
   end
 
   def update
+    @item = Item.find(params[:id])
+    if @item.update(item_params)
+      redirect_to item_path(@item.id)
+    else
+      render :edit
+    end
   end
 
   private

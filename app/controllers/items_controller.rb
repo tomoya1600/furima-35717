d111d12be57fd10a9d5bacb9cad6d02abbf14912
tomoya1600 <on100.ogata.tomoya@gmail.com>
2021-06-08 18:21:27 +0900

    必要表示情報の修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,11 +1,14 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
+
   def index
     @items = Item.order("created_at DESC")
   end
+
   def new
     @item = Item.new
   end
+
   def create
     @item = Item.new(item_params)
     if @item.save
@@ -14,9 +17,18 @@ class ItemsController < ApplicationController
       render :new
     end
   end
+
   def show
     @item = Item.find params[:id]
   end
+
+  def edit
+    @item = Item.find(params[:id])
+  end
+
+  def update
+  end
+
   private
 
   def item_params

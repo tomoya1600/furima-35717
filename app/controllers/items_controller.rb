8ce61dd60700e9580f4b2d3891b4c119eb5e19eb
tomoya1600 <on100.ogata.tomoya@gmail.com>
2021-06-08 19:09:50 +0900

    unlessメソッドで条件化

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -23,7 +23,9 @@ class ItemsController < ApplicationController
   end
 
   def edit
-    redirect_to root_path if current_user.id != @item.user_id
+    unless user_signed_in? && current_user.id == @item.user_id
+      redirect_to root_path
+    end
   end
 
   def update

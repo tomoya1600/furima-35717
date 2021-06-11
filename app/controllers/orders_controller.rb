0e607c1d1a7989a716e9788d5f08a890b2899019
tomoya1600 <on100.ogata.tomoya@gmail.com>
2021-06-11 17:46:15 +0900

    購入ページのビューファイル修正

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,8 +1,31 @@
 class OrdersController < ApplicationController
+  before_action :authenticate_user!
+  before_action :set_item, only: [:index, :create]
+
+
   def index
     @order_address = OrderAddress.new
   end
 
   def create
+    @order_address = OrderAddress.new(order_params)
+    if @order_address.valid?
+      @order_address.save
+      return redirect_to root_path
+    else
+      render action: :index
+    end
   end
+
+  private
+
+  def set_item
+    @item = Item.find(params[:item_id])
+  end
+
+  def order_params
+    params.require(:order_address).permit(:postal_code, :prefecture_id, :city_name, :block_name, :building_name, :phone_number).merge(token: params[:token], item_id: params[:item_id], user_id: current_user.id)
+  end
+
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,6 @@ Rails.application.routes.draw do
   get 'items/index'
   root to: "items#index"
   resources :items do
-    resources :orders, only: [:index]
+    resources :orders, only: [:index, :create]
   end
 end

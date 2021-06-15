bd8dfe8e724344424799e2b03e0504ad6f055503
tomoya1600 <on100.ogata.tomoya@gmail.com>
2021-06-15 16:57:01 +0900

    コード修正

--- app/models/order_address.rb
+++ app/models/order_address.rb
@@ -10,8 +10,8 @@ class OrderAddress
     validates :item_id
     validates :phone_number, format: { with: /\A\d{10}$|^\d{11}\z/ }
     validates :postal_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
+    validates :prefecture_id, numericality: { other_than: 1, message: "can't be blank"}
   end
-  validates :prefecture_id, numericality: {other_than: 0, message: "can't be blank"}
 
   def save
     order = Order.create(user_id: user_id, item_id: item_id)
